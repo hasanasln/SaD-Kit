eaaaca45925c0cfa840c1ad2c3685dc3ef4e2cf1
SaD-RaZaM <79222692+SaD-RaZaM@users.noreply.github.com>
2021-06-23 22:26:54 +0300

    Update phishing.sh

--- files/phishing.sh
+++ files/phishing.sh
@@ -7,7 +7,9 @@ printf "\e[34m
 •        [0] NexPhisher                           •
 •        [1] Zphisher                             •
 •        [2] PHISHING-ATTACK                      •
-•                                                 •
+•        [3] Turk-Sploit                          •
+•        [4] ShellPhish                           •
+•        [5] BlackEye                             •
 •        [x] Çıkış Yapmak İçin                    •
 •                                                 •
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -22,4 +24,14 @@ elif [[ $secim == 1 ]];then
 elif [[ $secim == 2 ]];then
 	cd
 	git clone https://github.com/termuxxtoolss/PHISHING-ATTACK
+elif [[ $secim == 3 ]];then
+     cd 
+     git clone https://github.com/DoughBoiKush/Turk-Sploit
+elif [[ $secim == 4 ]];then
+      cd 
+      git clone https://github.com/AbirHasan2005/ShellPhish
+elif [[ $secim == 5 ]];then
+      cd 
+      git clone https://github.com/An0nUD4Y/blackeye
+
 fi 
